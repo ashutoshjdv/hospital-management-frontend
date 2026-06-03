fadd41c2cb3d5ad0abec7f5272d25004445c48e1
ashutosh jadhav <jdvoct02@gmail.com>
2026-06-03 17:48:21 +0530

    frontend setup

---
+++ hospital-management/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './styles/globals.css'
+
+function App() {
+
+  
+
+  return (
+    <>
+      <div className=" bg-black text-white text-5xl p-10">
+      Tailwind Working
+    </div>
+    </>
+  )
+}
+
+export default App
